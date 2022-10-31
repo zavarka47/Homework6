b83730f00e2d0e4b25d0eae818c61a803a340963
Zavarka47 <pazuzu1995@gmail.com>
2022-10-31 21:41:37 +0300

    Commit for pull request

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.text.Format;
 public class Main {
     public static void main(String[] args) {
 
+
         System.out.println("Exercise 1.1");
         //С помощью цикла for выведите в консоль все числа от 1 до 10
         for (int i11 = 1; i11<=10; i11=i11+1){
